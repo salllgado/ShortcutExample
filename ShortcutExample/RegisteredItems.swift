ad08dc9f139494993fd12c449fd40ea92a052a28
Chrystian Salgado <cmartins@vidalink.com.br>
2020-04-07 17:29:33 -0300

    little updates

---
+++ ShortcutExample/RegisteredItems.swift
@@ -0,0 +1,27 @@
+//
+//  RegisteredItems.swift
+//  ShortcutExample
+//
+//  Created by Chrystian on 07/04/20.
+//  Copyright © 2020 Salgado Solutions. All rights reserved.
+//
+
+import Foundation
+
+enum RegisteredItems: String {
+    case secondViewController = "secondScreenDynamic"
+    
+    func registerNewItem() {
+        switch self {
+        case .secondViewController:
+            let userInfo = ["identifier": "someID"]
+            let shortcutItem = UIApplicationShortcutItem(type: self.rawValue, localizedTitle: "Red", localizedSubtitle: nil, icon: .init(type: .favorite), userInfo: userInfo as [String : NSSecureCoding])
+            
+            var shortcutItems = UIApplication.shared.shortcutItems?.filter({ (item) -> Bool in
+                item.type != self.rawValue
+            })
+            shortcutItems?.append(shortcutItem)
+            UIApplication.shared.shortcutItems = shortcutItems
+        }
+    }
+}g
--- ShortcutExample/ShortcutFactory.swift
+++ ShortcutExample/ShortcutFactory.swift
@@ -8,24 +8,6 @@
 
 import UIKit
 
-enum RegisteredItems: String {
-    case secondViewController = "secondScreenDynamic"
-    
-    func registerNewItem() {
-        switch self {
-        case .secondViewController:
-            let userInfo = ["identifier": "someID"]
-            let shortcutItem = UIApplicationShortcutItem(type: self.rawValue, localizedTitle: "Red", localizedSubtitle: nil, icon: .init(type: .favorite), userInfo: userInfo as [String : NSSecureCoding])
-            
-            var shortcutItems = UIApplication.shared.shortcutItems?.filter({ (item) -> Bool in
-                item.type != self.rawValue
-            })
-            shortcutItems?.append(shortcutItem)
-            UIApplication.shared.shortcutItems = shortcutItems
-        }
-    }
-}
-
 final class ShortcutFactory {
     
     let presenter: UINavigationController
